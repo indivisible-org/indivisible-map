a86f281f62c740b07c6fdb09d4696125d5f92569
meganr <meganrm@gmail.com>
2017-12-03 17:25:21 -0800

    moved map state to props

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,80 +1,46 @@
-import React from "react";
+import React from 'react';
 
 class MapView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      featuresHome: {
-        type: "FeatureCollection",
-        features: [
-          {
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [-122.3321, 47.6062]
-            },
-            properties: {
-              icon: "host-group"
-            }
-          },
-          {
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [-76.609383, 39.299236]
-            },
-            properties: {
-              icon: "issue"
-            }
-          },
-          {
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [-117.071869, 32.774799]
-            },
-            properties: {
-              icon: "challenge"
-            }
-          }
-        ]
-      }
-    };
+  }
+
+  componentDidMount() {
+    this.initializeMap();
   }
 
   initializeMap() {
     mapboxgl.accessToken =
-      "pk.eyJ1IjoiYWxhbjA0MCIsImEiOiJjamFrNm81dWkyZzMzMnhsZTI3bjR3eDVoIn0.K3FKPy6S_PwzjDjb02aGHA";
-    const style_url = "mapbox://styles/alan040/cjaqgutcnhdun2slmi3xbhgu1";
+      'pk.eyJ1IjoiYWxhbjA0MCIsImEiOiJjamFrNm81dWkyZzMzMnhsZTI3bjR3eDVoIn0.K3FKPy6S_PwzjDjb02aGHA';
+    const styleUrl = 'mapbox://styles/alan040/cjaqgutcnhdun2slmi3xbhgu1';
 
     const map = new mapboxgl.Map({
-      container: "map",
-      style: style_url
+      container: 'map',
+      style: styleUrl,
     });
 
     const addLayer = () => {
       map.addLayer(
         {
-          id: "townhall-points",
-          type: "symbol",
+          id: 'townhall-points',
+          type: 'symbol',
           source: {
-            type: "geojson",
-            data: this.state.featuresHome
+            type: 'geojson',
+            data: this.props.featuresHome,
           },
           layout: {
-            "icon-image": "{icon}",
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "icon-ignore-placement": true,
-            "icon-ignore-placement": true,
-            "icon-offset": {
+            'icon-image': '{icon}',
+            'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+            'icon-ignore-placement': true,
+            'icon-offset': {
               base: 1,
-              stops: [[0, [0, -15]], [10, [0, -10]], [12, [0, 0]]]
-            }
-          }
+              stops: [[0, [0, -15]], [10, [0, -10]], [12, [0, 0]]],
+            },
+          },
         },
-        "district_interactive"
+        'district_interactive',
       );
-    }
+    };
 
     // Set Mapbox map controls
     map.addControl(new mapboxgl.NavigationControl());
@@ -83,15 +49,13 @@ class MapView extends React.Component {
     map.touchZoomRotate.disableRotation();
 
     // map on 'load'
-    map.on("load", function() {
+    map.on('load', () => {
       map.fitBounds([[-128.8, 23.6], [-65.4, 50.2]]);
       addLayer();
     });
   }
 
-  componentDidMount() {
-    this.initializeMap();
-  }
+
   render() {
     return (
       <div>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,76 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import MapView from './components/MapView';
+import getData from './logics/getData';
 
 import './style/app.scss';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getEvents = this.getEvents.bind(this);
+    this.state = {
+      events: [],
+      groups: [],
+      featuresHome: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [-122.3321, 47.6062],
+            },
+            properties: {
+              icon: 'host-group',
+            },
+          },
+          {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [-76.609383, 39.299236],
+            },
+            properties: {
+              icon: 'issue',
+            },
+          },
+          {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [-117.071869, 32.774799],
+            },
+            properties: {
+              icon: 'challenge',
+            },
+          },
+        ],
+      },
+    };
+  }
 
-    constructor(props) {
-        super(props);
-    }
+  getEvents() {
+    const url = 'https://indivisible.actionkit.com/rest/v1/event/?name=local-indivisible-actions&is_private=false';
+    getData(url).then((result) => {
+      const events = result['u"objects"'];
+      this.setState({ events });
+    });
+  }
 
-    render() {
-        return (
-            <div>
+  render() {
+    return (
+      <div>
                 Hello World
-                <MapView />
-            </div>
-        )
-    }
-
+        <MapView
+          getEvents={this.getEvents}
+          events={this.state.events}
+          groups={this.state.groups}
+          featuresHome={this.state.featuresHome}
+        />
+      </div>
+    );
+  }
 }
 
-ReactDom.render(<App/>, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('root'));
