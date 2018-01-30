4b5793992d1a37e4d62acd1d234beb56644c550d
meganr <meganrm@gmail.com>
2018-01-29 16:40:28 -0800

    changed filters to selections, to be more general about user selections

---
+++ src/state/selections/reducers.js
@@ -0,0 +1,37 @@
+const initialState = {
+  filterValue: '',
+  location: '',
+  distance: 80467.2,
+  filterBy: 'all',
+  zipcode: '',
+};
+
+const filtersReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        filterValue: payload,
+      };
+    case 'SET_DISTANCE':
+      return {
+        ...state,
+        distance: payload,
+      };
+    case 'SET_LAT_LNG':
+      console.log('setting lat log', payload);
+      return {
+        ...state,
+        location: payload,
+      };
+    case 'SORT_BY':
+      return {
+        ...state,
+        filterBy: payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default filtersReducer;
---
+++ src/state/selections/selectors.js
@@ -0,0 +1,4 @@
+export const getDistance = state => state.selections.distance;
+export const getLocation = state => state.selections.location;
+export const getFilterBy = state => state.selections.filterBy;
+export const getFilterValue = state => state.selections.filterValue;
