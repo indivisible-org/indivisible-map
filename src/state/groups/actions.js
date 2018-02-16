1455fdaaa21c118138294366a45d74d36fbd30f1
meganr <meganrm@gmail.com>
2018-02-16 00:06:40 -0800

    batch reqesting groups

--- src/state/groups/actions.js
+++ src/state/groups/actions.js
@@ -11,11 +11,33 @@ export const setFeaturesHome = featuresHome => ({
   featuresHome,
 });
 
+let requested = 0;
+let newId = 0;
+const batch = 500;
+
+const requestNext = url => dispatch => getData(url).then((result) => {
+  const response = result.body;
+  const groups = Object.keys(response).map(id => response[id]);
+  newId = groups[groups.length - 1].id;
+  requested += groups.length;
+  dispatch(setGroups(groups));
+  if (groups.length >= batch) {
+    const nextUrl = `${firebaseUrl}/indivisible_groups.json?orderBy="$key"&startAt="${newId}"&limitToFirst=${batch}`;
+    dispatch(requestNext(nextUrl));
+  }
+});
+
 export const startSetGroups = () => (dispatch) => {
-  const url = `${firebaseUrl}/indivisible_groups.json`;
+  const url = `${firebaseUrl}/indivisible_groups.json?orderBy="id"&limitToFirst=${batch}`;
   return getData(url).then((result) => {
-    const response = JSON.parse(result.text);
+    const response = result.body;
     const groups = Object.keys(response).map(id => response[id]);
-    return dispatch(setGroups(groups));
+    newId = groups[groups.length - 1].id;
+    requested += groups.length;
+    dispatch(setGroups(groups));
+    if (requested < 6068) {
+      const nextUrl = `${firebaseUrl}/indivisible_groups.json?orderBy="$key"&startAt="${newId}"&limitToFirst=${batch}`;
+      dispatch(requestNext(nextUrl));
+    }
   });
 };
