5e5c3e8f09f1976a56fc121b5a793b7cff455efe
Alan Magelssen <alanmagelssen@gmail.com>
2018-04-06 19:55:29 -0700

    update with isSameOrAfter

--- src/state/events/actions.js
+++ src/state/events/actions.js
@@ -23,7 +23,7 @@ export const startSetEvents = () => (dispatch) => {
     const events = Object.keys(allevents)
       .map(id => new IndEvent(allevents[id]))
       .filter(evnt => moment(evnt.starts_at).isAfter())
-      .sort((a, b) => (moment(a.starts_at).isAfter(moment(b.starts_at))) ? 1 : -1)
+      .sort((a, b) => (moment(a.starts_at).isSameOrAfter(moment(b.starts_at))) ? 1 : -1)
     return (dispatch(setEvents(events)));
   });
 };
