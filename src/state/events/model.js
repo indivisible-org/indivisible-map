7b392ee681b4d918e365cfe8213bf01a3a00024e
meganrm <meganrm@gmail.com>
2018-07-05 13:53:25 -0700

    updating rsvp urls

--- src/state/events/model.js
+++ src/state/events/model.js
@@ -1,6 +1,19 @@
+const campaignMapping = {
+  15: 'local-actions',
+  21: 'save-scotus-actions',
+  9: 'recess-townhall',
+};
+
 export default class IndEvent {
   constructor(props) {
     Object.assign(this, props);
-    this.rsvpHref = `http://act.indivisible.org/event/local-actions/${this.id}/signup/`;
+    this.rsvpHref = this.makeUrl();
+  }
+
+  makeUrl() {
+    const arr = this.campaign.split('/');
+    const campaignNumber = arr[arr.length - 2];
+    const campaignName = campaignMapping[campaignNumber] || 'local-actions';
+    return `http://act.indivisible.org/event/${campaignName}/${this.id}/signup/`
   }
 }
