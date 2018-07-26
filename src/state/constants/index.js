3c57aef3882601218a4bf02ce32a12805429bfd9
meganrm <meganrm@gmail.com>
2018-07-26 14:17:01 -0700

    testing url

--- src/state/constants/index.js
+++ src/state/constants/index.js
@@ -1,3 +1,3 @@
 export const firebaseUrl = 'https://townhallproject-86312.firebaseio.com';
-export const indivisibleUrl = 'https://indivisible-data.firebaseio.com';
+export const indivisibleUrl = 'https://indivisible-testing-data.firebaseio.com';
 export const townhallevents = 'Town Hall';
