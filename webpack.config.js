815ee10a8ef91603d8b65df05473c59ae8265e7d
meganrm <meganrm@gmail.com>
2020-08-03 23:13:47 -0700

    removing uglify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const HTMLPlugin = require('html-webpack-plugin');
 
 // Makes a separate CSS bundle
 const ExtractPlugin = require('extract-text-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const { EnvironmentPlugin, DefinePlugin } = require('webpack');
 
@@ -17,15 +16,6 @@ const plugins = [
   }),
   new ExtractPlugin('bundle.[hash].css'),
   new EnvironmentPlugin(['NODE_ENV']),
-  new UglifyJSPlugin({
-    uglifyOptions:
-                {
-                  compress: {
-                    warnings: false,
-                  },
-                  sourceMap: true,
-                },
-  }),
   new DefinePlugin({
     __AUTH_URL__: JSON.stringify(process.env.AUTH_URL),
     __API_URL__: JSON.stringify(process.env.API_URL),
